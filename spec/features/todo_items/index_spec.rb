680a57f205a6a2a3c967d21aca489b46317d9c2b
Izabella Konstanciak <i.konstanciak@gmail.com>
2015-11-04 16:33:41 +0100

    Add todo items controller

---
+++ spec/features/todo_items/index_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.configure do |c|
+  c.expose_current_running_example_as :example
+end
+
+describe "Viewing todo items" do
+  let!(:todo_list) { TodoList.create(title: "Grocery list", description:"Groceries") }
+  it "Displays no items when a todo list is empty" do
+    visit "/todo_lists"
+    within "#todo_list_#{todo_list.id}" do
+      click_link "List Items"
+    end
+    expect(page).to have_content("TodoItems#index")
+  end
+  
+end
